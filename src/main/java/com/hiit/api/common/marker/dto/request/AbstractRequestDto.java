0623211b5bbb1394a95a5fe8ce9c4baf73f65e7f
belljun3395 <195850@jnu.ac.kr>
2023-07-26 18:59:34 +0900

    HIIT-000 feat: AbstractRequestDto 선언

---
+++ src/main/java/com/hiit/api/common/marker/dto/request/AbstractRequestDto.java
@@ -0,0 +1,6 @@
+package com.hiit.api.common.marker.dto.request;
+
+import com.hiit.api.common.marker.dto.AbstractDto;
+
+/** 해당 계층에게 전달된 요청을 구분하기 하기 위한 마커인터페이스 */
+public interface AbstractRequestDto extends AbstractDto {}
