8dd3a4cdd765e4fcb7d3ad8e994dbafffae1f5a2
belljun3395 <195850@jnu.ac.kr>
2023-08-03 21:34:27 +0900

    HIIT-000 feat: MessageCode 구현

---
+++ src/main/java/com/hiit/api/web/support/MessageCode.java
@@ -0,0 +1,26 @@
+package com.hiit.api.web.support;
+
+/** 메시지 코드 */
+public enum MessageCode {
+	/** 성공 메시지 코드 */
+	SUCCESS("success", "성공"),
+	/** 삭제 메시지 코드 */
+	RESOURCE_DELETED("resource.deleted", "삭제되었습니다."),
+	/** 생성 메시지 코드 */
+	RESOURCE_CREATED("resource.created", "새로 생성되었습니다.");
+	private final String code;
+	private final String value;
+
+	MessageCode(String code, String value) {
+		this.code = code;
+		this.value = value;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+}
