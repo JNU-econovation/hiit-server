8170c8bc30b9fda0c52a04a451ab62b478c16ac3
belljun3395 <195850@jnu.ac.kr>
2023-08-20 17:47:08 +0900

    HIIT-000 feat: 엔티티 편의 메서드 touch 구현

--- src/main/java/com/hiit/api/repository/entity/business/supporter/FooEntitySupporter.java
+++ src/main/java/com/hiit/api/repository/entity/business/supporter/FooEntitySupporter.java
@@ -1,12 +1,22 @@
 package com.hiit.api.repository.entity.business.supporter;
 
 import com.hiit.api.repository.entity.business.FooEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class FooEntitySupporter {
 
 	public FooEntity getIdEntity(Long id) {
 		return FooEntity.builder().id(id).build();
+	}
+
+	public FooEntity touch(FooEntity source) {
+		Long count = source.getCount();
+		if (count.compareTo(0L) <= 0) {
+			throw new RuntimeException("count is under zero");
+		}
+		return source.toBuilder().count(count - 1).build();
 	}
 }
