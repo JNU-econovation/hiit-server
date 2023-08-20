dd16354250a69cebb7b95062db5cf9d19f5480d4
belljun3395 <195850@jnu.ac.kr>
2023-08-20 17:50:23 +0900

    HIIT-000 refactor: touch 편의 메서드 제거 반영

--- src/main/java/com/hiit/api/domain/usecase/foo/CountFooService.java
+++ src/main/java/com/hiit/api/domain/usecase/foo/CountFooService.java
@@ -2,6 +2,7 @@ package com.hiit.api.domain.usecase.foo;
 
 import com.hiit.api.repository.dao.bussiness.FooRepository;
 import com.hiit.api.repository.entity.business.FooEntity;
+import com.hiit.api.repository.entity.business.supporter.FooEntitySupporter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,12 +15,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class CountFooService {
 
 	private final FooRepository fooRepository;
+	private final FooEntitySupporter fooEntitySupporter;
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public Long execute(Long id) {
 		FooEntity fooEntity =
 				fooRepository.findById(id).orElseThrow(() -> new RuntimeException("Foo not found"));
-		fooEntity.touch();
-		return fooEntity.getCount();
+		FooEntity updateFooEntity = fooEntitySupporter.touch(fooEntity);
+		return fooRepository.save(updateFooEntity).getCount();
 	}
 }
