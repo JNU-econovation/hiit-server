6bdf3594c057bd49704c2345edba23745f4cc42b
belljun3395 <195850@jnu.ac.kr>
2023-10-05 14:36:34 +0900

    HIIT-000 feat: AbstractDomainConverter 선언

---
+++ src/main/java/com/hiit/api/domain/support/AbstractDomainConverter.java
@@ -0,0 +1,32 @@
+package com.hiit.api.domain.support;
+
+import com.hiit.api.common.marker.dto.AbstractDto;
+import com.hiit.api.common.marker.entity.EntityMarker;
+import com.hiit.api.common.support.converter.AbstractConverter;
+
+/**
+ * 엔티티를 도메인으로 변환하기 위한 서비스 계층 컨버터 인터페이스
+ *
+ * @param <T> 엔티티 타입
+ * @param <R> 도메인 타입
+ * @param <K> 컨트롤러 계층에 전달할 Dto 타입
+ */
+public interface AbstractDomainConverter<T extends EntityMarker, R, K extends AbstractDto>
+		extends AbstractConverter<T, R> {
+
+	/**
+	 * 엔티티 타입을 도메인으로 변환한다
+	 *
+	 * @param t DTO 타입의 객체
+	 * @return 도메인
+	 */
+	R from(T t);
+
+	/**
+	 * 도메인을 Dto로 변환한다.
+	 *
+	 * @param r 도메인 객체
+	 * @return Dto 타입
+	 */
+	K to(R r);
+}
