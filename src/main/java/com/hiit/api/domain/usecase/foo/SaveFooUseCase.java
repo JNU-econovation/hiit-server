36ca929da1ed0c9a0c75960aaaf6bab6796ab3c5
belljun3395 <195850@jnu.ac.kr>
2023-10-05 14:45:31 +0900

    HIIT-000 refactor: UseCase 변경사항 반영

--- src/main/java/com/hiit/api/domain/usecase/foo/SaveFooUseCase.java
+++ src/main/java/com/hiit/api/domain/usecase/foo/SaveFooUseCase.java
@@ -1,11 +1,14 @@
 package com.hiit.api.domain.usecase.foo;
 
-import com.hiit.api.domain.dto.response.FooUseCaseResponse;
+import com.hiit.api.common.marker.dto.response.ServiceResponse;
+import com.hiit.api.common.marker.entity.EntityMarker;
 import com.hiit.api.domain.model.Foo;
 import com.hiit.api.domain.service.foo.FooService;
 import com.hiit.api.domain.support.foo.converter.FooConverter;
 import com.hiit.api.domain.usecase.AbstractUseCase;
 import com.hiit.api.web.dto.request.SaveFooRequest;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,7 +21,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class SaveFooUseCase implements AbstractUseCase<SaveFooRequest, FooUseCaseResponse> {
+public class SaveFooUseCase implements AbstractUseCase<SaveFooRequest> {
 
 	/** 다른 계층 혹은 다른 도메인과 연동이 필요한 경우 사용한다. */
 	private final FooService fooService;
@@ -26,13 +29,25 @@ public class SaveFooUseCase implements AbstractUseCase<SaveFooRequest, FooUseCas
 	private final FooConverter fooConverter;
 
 	@Override
-	public FooUseCaseResponse execute(SaveFooRequest request) {
-		Foo foo = fooConverter.from(request);
+	public ServiceResponse execute(SaveFooRequest request) {
+
+		Foo foo = fooConverter.from(SampleEntity.of(request));
 
 		fooService.doA(foo);
 		fooService.doB(foo);
 		fooService.doC(foo);
 
-		return fooConverter.toDomainResponse(foo);
+		return fooConverter.to(foo);
+	}
+
+	// 임시 엔티티 객체입니다.
+	@Builder
+	@Getter
+	public static class SampleEntity implements EntityMarker {
+		private String name;
+
+		public static SampleEntity of(SaveFooRequest source) {
+			return SampleEntity.builder().name(source.getName()).build();
+		}
 	}
 }
