5e5cbfc5897cf494596057dbc42430394c385595
belljun3395 <195850@jnu.ac.kr>
2023-10-09 22:34:34 +0900

    HIIT-001 feat: 알림관련 요청 구현

---
+++ src/main/java/com/hiit/api/web/dto/request/noti/MemberItNotiRequest.java
@@ -0,0 +1,20 @@
+package com.hiit.api.web.dto.request.noti;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberItNotiRequest {
+
+	private Long iid;
+	private Boolean notiStatus;
+}
