9f45013f9c4b1b12ed455dc01fa72cbae7ee856b
belljun3395 <195850@jnu.ac.kr>
2023-10-16 18:53:15 +0900

    HIIT-001 refactor: ItResponse를 상속하여 구현한 Response 수정

--- src/main/java/com/hiit/api/domain/dto/response/it/DayItResponse.java
+++ src/main/java/com/hiit/api/domain/dto/response/it/DayItResponse.java
@@ -2,19 +2,25 @@ package com.hiit.api.domain.dto.response.it;
 
 import com.hiit.api.common.marker.dto.response.ServiceResponse;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import lombok.experimental.SuperBuilder;
 
 @Getter
 @ToString
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
-@SuperBuilder(toBuilder = true)
-public class DayItResponse extends ItResponse implements ServiceResponse {
+@Builder(toBuilder = true)
+public class DayItResponse implements ServiceResponse {
 
 	private Long day;
+
+	private Long id;
+	private String topic;
+	private Long startTime;
+	private Long endTime;
+	private Long participatePerson;
 }
--- src/main/java/com/hiit/api/domain/dto/response/it/MemberInItResponse.java
+++ src/main/java/com/hiit/api/domain/dto/response/it/MemberInItResponse.java
@@ -2,19 +2,25 @@ package com.hiit.api.domain.dto.response.it;
 
 import com.hiit.api.common.marker.dto.response.ServiceResponse;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import lombok.experimental.SuperBuilder;
 
 @Getter
 @ToString
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
-@SuperBuilder(toBuilder = true)
-public class MemberInItResponse extends ItResponse implements ServiceResponse {
+@Builder(toBuilder = true)
+public class MemberInItResponse implements ServiceResponse {
 
 	private Boolean memberIn;
+
+	private Long id;
+	private String topic;
+	private Long startTime;
+	private Long endTime;
+	private Long participatePerson;
 }
