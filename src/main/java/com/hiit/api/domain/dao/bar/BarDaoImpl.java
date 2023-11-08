f932271ab8df66dff98b6554bed56370c78580dd
belljun3395 <195850@jnu.ac.kr>
2023-11-08 16:23:31 +0900

    HIIT-000 chore: 사용하지 않는 임포트문 정리
    - BarDaoImpl

--- src/main/java/com/hiit/api/domain/dao/bar/BarDaoImpl.java
+++ src/main/java/com/hiit/api/domain/dao/bar/BarDaoImpl.java
@@ -5,7 +5,6 @@ import com.hiit.api.domain.dao.AbstractJpaDao;
 import com.hiit.api.repository.entity.business.BarEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 /** 연관관계를 가지고 있는 엔티티의 경우 신경쓸 것이 더 많다. */
 @Repository
