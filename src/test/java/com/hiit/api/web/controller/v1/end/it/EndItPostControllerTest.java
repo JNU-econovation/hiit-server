2cc9f7c8a2fcba69e9cf4326e465fb46487e7e3a
belljun3395 <195850@jnu.ac.kr>
2023-11-22 21:32:49 +0900

    HIIT-004 test: EndItPostControllerTest 추가

--- src/test/java/com/hiit/api/web/controller/v1/end/it/EndItPostControllerTest.java
+++ src/test/java/com/hiit/api/web/controller/v1/end/it/EndItPostControllerTest.java
@@ -1,9 +1,8 @@
-package com.hiit.api.web.controller.v1.member;
+package com.hiit.api.web.controller.v1.end.it;
 
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
-import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
@@ -11,8 +10,7 @@ import com.epages.restdocs.apispec.Schema;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hiit.api.AppMain;
 import com.hiit.api.web.controller.description.Description;
-import com.hiit.api.web.controller.description.MemberDescription;
-import com.hiit.api.web.dto.request.noti.MemberItNotiRequest;
+import com.hiit.api.web.dto.request.end.it.EditEndIpRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,41 +25,40 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureRestDocs
 @AutoConfigureMockMvc(addFilters = false)
 @SpringBootTest(classes = AppMain.class)
-class MemberPostControllerTest {
+class EndItPostControllerTest {
+
 	@Autowired private MockMvc mockMvc;
 	@Autowired private ObjectMapper objectMapper;
-	private static final String TAG = "Member";
-	private static final String BASE_URL = "/api/v1/members";
+	private static final String TAG = "EndIt-Controller";
+	private static final String BASE_URL = "/api/v1/end/its";
 
 	@Test
-	@DisplayName(BASE_URL + "/{mid}/noti/its")
-	void noti() throws Exception {
+	@DisplayName(BASE_URL)
+	void editEndIt() throws Exception {
+		// set service mock
 
-		MemberItNotiRequest request = MemberItNotiRequest.builder().iid(1L).notiStatus(true).build();
+		EditEndIpRequest request = EditEndIpRequest.builder().id(1L).title("종료잇 수정 제목").build();
 
 		String content = objectMapper.writeValueAsString(request);
 
-		// set service mock
-
 		mockMvc
 				.perform(
-						post(BASE_URL + "/{mid}/noti/its", 1)
+						put(BASE_URL, 0)
 								.content(content)
 								.header("Authorization", "{{accessToken}}")
 								.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().is2xxSuccessful())
 				.andDo(
 						document(
-								"MemberItNoti",
+								"EditEndItInfo",
 								resource(
 										ResourceSnippetParameters.builder()
-												.description("멤버에 잇 알림 설정")
+												.description("종료 잇 수정한다.")
 												.tag(TAG)
-												.requestSchema(Schema.schema("MemberItNotiRequest"))
-												.pathParameters(parameterWithName("mid").description("member id"))
+												.requestSchema(Schema.schema("EditEndItInfoRequest"))
 												.requestHeaders(Description.authHeader())
-												.responseSchema(Schema.schema("MemberItNotiResponse"))
-												.responseFields(Description.success(MemberDescription.withOutData()))
+												.responseSchema(Schema.schema("EditEndItInfoResponse"))
+												.responseFields(Description.success())
 												.build())));
 	}
 }
