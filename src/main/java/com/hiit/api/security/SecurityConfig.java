373c8d254a0203c617989a45b09172eedde2e3ae
belljun3395 <195850@jnu.ac.kr>
2023-11-27 13:11:09 +0900

    HIIT-006 feat: webSecurityFilterIgnoreCustomizer를 통해 특정 주소 필터 거치지 않도록 구현

--- src/main/java/com/hiit/api/security/SecurityConfig.java
+++ src/main/java/com/hiit/api/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
@@ -128,5 +129,10 @@ public class SecurityConfig {
 	 */
 	public OncePerRequestFilter getTokenInvalidExceptionHandlerFilter() {
 		return new TokenInvalidExceptionHandlerFilter();
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityFilterIgnoreCustomizer() {
+		return web -> web.ignoring().antMatchers("/api/v1/token");
 	}
 }
