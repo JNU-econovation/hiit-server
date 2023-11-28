0f9319bcbceed0c2a8ac34d39a4bef3bc1538179
belljun3395 <195850@jnu.ac.kr>
2023-11-28 23:06:03 +0900

    HIIT-000 feat: MessageCode#RESOURCE_UPDATED 추가

--- src/main/java/com/hiit/api/web/support/MessageCode.java
+++ src/main/java/com/hiit/api/web/support/MessageCode.java
@@ -6,6 +6,8 @@ public enum MessageCode {
 	SUCCESS("success", "성공"),
 	/** 삭제 메시지 코드 */
 	RESOURCE_DELETED("resource.deleted", "삭제되었습니다."),
+	/** 수정 메시지 코드 */
+	RESOURCE_UPDATED("resource.updated", "수정되었습니다."),
 	/** 생성 메시지 코드 */
 	RESOURCE_CREATED("resource.created", "새로 생성되었습니다.");
 	private final String code;
