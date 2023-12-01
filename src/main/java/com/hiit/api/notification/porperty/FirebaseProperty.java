527683fba91aca7da0da8cb0ecca7a793e3524d8
belljun3395 <195850@jnu.ac.kr>
2023-12-01 10:21:06 +0900

    HIIT-*** feat: FirebaseProperty 구현

---
+++ src/main/java/com/hiit/api/notification/porperty/FirebaseProperty.java
@@ -0,0 +1,33 @@
+package com.hiit.api.notification.porperty;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@ToString
+@Component
+public class FirebaseProperty {
+
+	@Value("${firebase.multicast-message.size}")
+	Long multicastMessageSize;
+
+	@Value("${firebase.projectId:#{null}}")
+	String projectId;
+
+	@Value("${firebase.private.keyId:#{null}}")
+	String privateKeyId;
+
+	@Value("${firebase.private.key:#{null}}")
+	String privateKey;
+
+	@Value("${firebase.client.id:#{null}}")
+	String clientId;
+
+	@Value("${firebase.client.email:#{null}}")
+	String clientEmail;
+
+	@Value("${firebase.token.uri}")
+	String tokenUri;
+}
