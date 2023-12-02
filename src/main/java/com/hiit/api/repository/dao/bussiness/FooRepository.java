95e8e7b7c28ef010114dc71542856cbc00cd0d2c
belljun3395 <195850@jnu.ac.kr>
2023-12-03 00:57:55 +0900

    HIIT-014 remove: Foo, Bar 클래스 삭제

--- src/main/java/com/hiit/api/repository/dao/bussiness/FooRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hiit.api.repository.dao.bussiness;
-
-import com.hiit.api.repository.dao.bussiness.custom.FooCustomRepository;
-import com.hiit.api.repository.dao.bussiness.jpa.FooJpaRepository;
-import com.hiit.api.repository.entity.business.FooEntity;
-import org.springframework.data.repository.RepositoryDefinition;
-
-@RepositoryDefinition(domainClass = FooEntity.class, idClass = Long.class)
-public interface FooRepository extends FooJpaRepository, FooCustomRepository {}
