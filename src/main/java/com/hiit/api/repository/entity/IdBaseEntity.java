b8b56e27d6f30fe017d99ba1ebc9ec0f4c2155ec
belljun3395 <195850@jnu.ac.kr>
2023-12-03 16:35:21 +0900

    HIIT-014 feat: IdBaseEntity 구현

---
+++ src/main/java/com/hiit/api/repository/entity/IdBaseEntity.java
@@ -0,0 +1,39 @@
+package com.hiit.api.repository.entity;
+
+import com.hiit.api.repository.support.listener.SoftDeleteListener;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@MappedSuperclass
+@EntityListeners({AuditingEntityListener.class, SoftDeleteListener.class})
+@SuperBuilder(toBuilder = true)
+public class IdBaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Builder.Default
+	@Column(nullable = false)
+	private Boolean deleted = false;
+
+	public void delete() {
+		this.deleted = true;
+	}
+}
