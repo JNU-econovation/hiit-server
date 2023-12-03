8947d4c5bdc5b9f1eed13e1e0d9fa9c5501dc216
belljun3395 <195850@jnu.ac.kr>
2023-12-03 16:43:00 +0900

    HIIT-014 feat: InItRepository 구현

---
+++ src/main/java/com/hiit/api/repository/dao/bussiness/custom/InItCustomRepository.java
@@ -0,0 +1,21 @@
+package com.hiit.api.repository.dao.bussiness.custom;
+
+import com.hiit.api.repository.entity.business.it.DayCodeList;
+import com.hiit.api.repository.entity.business.it.InItEntity;
+import com.hiit.api.repository.entity.business.it.ItStatus;
+import com.hiit.api.repository.entity.business.member.HiitMemberEntity;
+import java.util.List;
+
+public interface InItCustomRepository {
+
+	// 특정 참여 잇과 연관된 잇 연관 엔티티를 모두 삭제한다.
+	void deleteByIdWithItRelation(Long id);
+
+	// 특정 타겟 아이디와 특정 상태, 특정 요일 코드를 가진 모든 잇을 조회한다.
+	List<InItEntity> findAllByTargetIdAndStatusAndDayCode(
+			Long targetId, ItStatus status, DayCodeList dayCode);
+
+	/** 특정 타겟 아이디와 특정 상태, 특정 멤버를 가진 잇을 조회한다. */
+	InItEntity findByTargetIdAndStatusAndHiitMember(
+			Long targetId, ItStatus status, HiitMemberEntity hiitMember);
+}
