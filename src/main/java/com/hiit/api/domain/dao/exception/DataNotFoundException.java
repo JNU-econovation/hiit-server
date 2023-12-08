e3c2c6f81fdab3c864053971ebcdb651f4b01618
belljun3395 <195850@jnu.ac.kr>
2023-12-08 12:21:45 +0900

    HIIT-021 feat: DataNotFoundException 구현

---
+++ src/main/java/com/hiit/api/domain/dao/exception/DataNotFoundException.java
@@ -0,0 +1,15 @@
+package com.hiit.api.domain.dao.exception;
+
+/** 데이터 조회 오류 예외 */
+public class DataNotFoundException extends RuntimeException {
+
+	private static final String DEFAULT_MESSAGE = "Data not found";
+
+	public DataNotFoundException() {
+		super(DEFAULT_MESSAGE);
+	}
+
+	public DataNotFoundException(String data) {
+		super(data + " " + DEFAULT_MESSAGE);
+	}
+}
