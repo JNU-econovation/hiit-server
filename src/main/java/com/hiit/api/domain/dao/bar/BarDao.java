e5cf063c92de2402a03c597630a23197f6d1b80f
belljun3395 <195850@jnu.ac.kr>
2023-12-08 15:13:05 +0900

    HIIT-021 remove: Foo, Bar 관련 엔티티 제거

--- src/main/java/com/hiit/api/domain/dao/bar/BarDao.java
+++
@@ -1,6 +0,0 @@
-package com.hiit.api.domain.dao.bar;
-
-import com.hiit.api.domain.dao.JpaDao;
-
-/** 연관관계를 가지고 있는 엔티티의 경우 신경쓸 것이 더 많다. */
-public interface BarDao extends JpaDao<BarData, Long> {}
--- src/main/java/com/hiit/api/repository/repository/bussiness/custom/BarCustomRepository.java
+++
@@ -1,3 +0,0 @@
-package com.hiit.api.repository.repository.bussiness.custom;
-
-public interface BarCustomRepository {}
