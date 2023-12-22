56d7d9028996a47f5f75aab4659a4bf4bd27ac56
belljun3395 <195850@jnu.ac.kr>
2023-12-22 17:45:06 +0900

    HIIT-017 feat: WithDao#findAllByInIt 메서드 추가

--- src/main/java/com/hiit/api/domain/dao/with/WithDao.java
+++ src/main/java/com/hiit/api/domain/dao/with/WithDao.java
@@ -4,6 +4,7 @@ import com.hiit.api.domain.dao.JpaDao;
 import com.hiit.api.domain.dao.support.PageData;
 import com.hiit.api.domain.dao.support.PageableInfo;
 import com.hiit.api.domain.dao.support.Period;
+import java.util.List;
 
 public interface WithDao extends JpaDao<WithData, Long> {
 
@@ -12,6 +13,8 @@ public interface WithDao extends JpaDao<WithData, Long> {
 	boolean existsById(Long id);
 
 	PageData<WithData> findAllByInIt(Long init, PageableInfo pageable, Long member);
+
+	List<WithData> findAllByInIt(Long init, Long member);
 
 	WithData findByInItEntityAndMemberAndPeriod(Long inIt, Long member, Period period);
 }
