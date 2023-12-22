670916ca3d22a969ecebb324e7500c79337150bc
belljun3395 <195850@jnu.ac.kr>
2023-12-22 23:33:32 +0900

    HIIT-019 feat: HIT 관련 구현 반영

--- src/main/java/com/hiit/api/web/controller/v1/hit/HitCommandController.java
+++ src/main/java/com/hiit/api/web/controller/v1/hit/HitCommandController.java
@@ -1,7 +1,8 @@
 package com.hiit.api.web.controller.v1.hit;
 
-import com.hiit.api.common.marker.dto.response.ServiceResponse;
+import com.hiit.api.domain.dto.request.hit.HitUseCaseRequest;
 import com.hiit.api.domain.dto.response.hit.HitInfo;
+import com.hiit.api.domain.usecase.hit.HitUseCase;
 import com.hiit.api.security.authentication.token.TokenUserDetails;
 import com.hiit.api.web.dto.request.hit.HitRequest;
 import com.hiit.api.web.support.ApiResponse;
@@ -23,11 +24,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class HitCommandController {
 
+	private final HitUseCase hitUseCase;
+
 	@PostMapping()
-	public ApiResponse<ApiResponse.SuccessBody<ServiceResponse>> hit(
+	public ApiResponse<ApiResponse.SuccessBody<HitInfo>> hit(
 			@AuthenticationPrincipal TokenUserDetails userDetails,
 			@Valid @RequestBody HitRequest request) {
-		HitInfo hit = HitInfo.builder().count(100L).build();
-		return ApiResponseGenerator.success(hit, HttpStatus.OK, MessageCode.SUCCESS);
+		HitInfo hit = null;
+		try {
+			Long memberId = Long.valueOf(userDetails.getUsername());
+			HitUseCaseRequest hitRequest =
+					HitUseCaseRequest.builder().memberId(memberId).withId(request.getId()).build();
+			hit = hitUseCase.execute(hitRequest);
+			if (hit == null) {
+				hit = getHitInfoMockResponse();
+				return ApiResponseGenerator.success(hit, HttpStatus.OK, MessageCode.SUCCESS);
+			}
+			return ApiResponseGenerator.success(hit, HttpStatus.OK, MessageCode.SUCCESS);
+		} catch (Exception e) {
+			hit = getHitInfoMockResponse();
+			return ApiResponseGenerator.success(hit, HttpStatus.OK, MessageCode.SUCCESS);
+		}
+	}
+
+	private HitInfo getHitInfoMockResponse() {
+		return HitInfo.builder().count(100L).build();
 	}
 }
