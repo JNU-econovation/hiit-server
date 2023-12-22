dc129185e0885c9a954452f9995e472827f395d3
belljun3395 <195850@jnu.ac.kr>
2023-12-22 23:32:09 +0900

    HIIT-019 feat: HitItTimeInfo 구현

---
+++ src/main/java/com/hiit/api/domain/service/hit/HitItTimeInfo.java
@@ -0,0 +1,20 @@
+package com.hiit.api.domain.service.hit;
+
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class HitItTimeInfo {
+	LocalTime startTime;
+	LocalTime endTime;
+}
