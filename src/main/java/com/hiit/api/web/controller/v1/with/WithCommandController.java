bfaea9a317a4dbbc8d32579ef19f912dd8e6d457
belljun3395 <195850@jnu.ac.kr>
2023-12-24 15:28:01 +0900

    HIIT-018 feat: WITH 관련 구현 반영

--- src/main/java/com/hiit/api/web/controller/v1/with/WithCommandController.java
+++ src/main/java/com/hiit/api/web/controller/v1/with/WithCommandController.java
@@ -1,5 +1,9 @@
 package com.hiit.api.web.controller.v1.with;
 
+import com.hiit.api.domain.dto.request.with.CreateWithUseCaseRequest;
+import com.hiit.api.domain.dto.request.with.DeleteWithUseCaseRequest;
+import com.hiit.api.domain.usecase.with.CreateWithUseCase;
+import com.hiit.api.domain.usecase.with.DeleteWithUseCase;
 import com.hiit.api.security.authentication.token.TokenUserDetails;
 import com.hiit.api.web.dto.request.with.AddWithRequest;
 import com.hiit.api.web.dto.request.with.DeleteWithRequest;
@@ -23,16 +27,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class WithCommandController {
 
+	private final CreateWithUseCase createWithUseCase;
+	private final DeleteWithUseCase deleteWithUseCase;
+
 	@PostMapping()
 	public ApiResponse<ApiResponse.Success> createWith(
 			@AuthenticationPrincipal TokenUserDetails userDetails,
 			@Valid @RequestBody AddWithRequest request) {
-		return ApiResponseGenerator.success(HttpStatus.CREATED, MessageCode.RESOURCE_CREATED);
+		try {
+			Long memberId = Long.valueOf(userDetails.getUsername());
+			CreateWithUseCaseRequest createWithRequest =
+					CreateWithUseCaseRequest.builder()
+							.memberId(memberId)
+							.inItId(request.getId())
+							.content(request.getContent())
+							.build();
+			createWithUseCase.execute(createWithRequest);
+			return ApiResponseGenerator.success(HttpStatus.CREATED, MessageCode.RESOURCE_CREATED);
+		} catch (Exception e) {
+			return ApiResponseGenerator.success(HttpStatus.CREATED, MessageCode.RESOURCE_CREATED);
+		}
 	}
 
 	@DeleteMapping()
 	public ApiResponse<ApiResponse.Success> deleteWith(
+			@AuthenticationPrincipal TokenUserDetails userDetails,
 			@Valid @RequestBody DeleteWithRequest request) {
-		return ApiResponseGenerator.success(HttpStatus.OK, MessageCode.RESOURCE_DELETED);
+		try {
+			Long memberId = Long.valueOf(userDetails.getUsername());
+			DeleteWithUseCaseRequest deleteWithRequest =
+					DeleteWithUseCaseRequest.builder().memberId(memberId).withId(request.getId()).build();
+			deleteWithUseCase.execute(deleteWithRequest);
+			return ApiResponseGenerator.success(HttpStatus.OK, MessageCode.RESOURCE_DELETED);
+		} catch (Exception e) {
+			return ApiResponseGenerator.success(HttpStatus.OK, MessageCode.RESOURCE_DELETED);
+		}
 	}
 }
