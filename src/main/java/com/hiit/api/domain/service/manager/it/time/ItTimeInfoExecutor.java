45d0376bcd5b096e94ca78ce28c086d1448e610e
belljun3395 <195850@jnu.ac.kr>
2024-01-02 22:49:15 +0900

    HIIT-030 refactor: domain.service 패키지 리펙토링

--- src/main/java/com/hiit/api/domain/service/manager/it/time/ItTimeInfoExecutor.java
+++
@@ -1,6 +0,0 @@
-package com.hiit.api.domain.service.manager.it.time;
-
-public interface ItTimeInfoExecutor {
-
-	ItTimeInfo execute(Long id);
-}
--- src/main/java/com/hiit/api/domain/service/with/WithInfoAssembleService.java
+++ src/main/java/com/hiit/api/domain/service/with/WithInfoAssembleService.java
@@ -1,46 +1,52 @@
 package com.hiit.api.domain.service.with;
 
 import com.hiit.api.domain.dao.hit.HitDao;
-import com.hiit.api.domain.dao.it.in.InItData;
-import com.hiit.api.domain.dao.support.PageData;
-import com.hiit.api.domain.dao.support.PageableInfo;
-import com.hiit.api.domain.dao.support.Period;
-import com.hiit.api.domain.dao.with.WithData;
+import com.hiit.api.domain.dto.PageRequest;
 import com.hiit.api.domain.dto.response.with.WithInfo;
 import com.hiit.api.domain.dto.response.with.WithMemberInfo;
+import com.hiit.api.domain.model.it.in.InIt;
+import com.hiit.api.domain.model.with.With;
+import com.hiit.api.domain.support.entity.PageElements;
+import com.hiit.api.domain.support.entity.Period;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
+/** WithInfo 생성을 위해 필요한 정보를 조합하는 서비스 */
 @Slf4j
-@Service
+@Component
 @RequiredArgsConstructor
 public class WithInfoAssembleService {
 
 	private final HitDao hitDao;
 
 	public PageImpl<WithInfo> execute(
-			PageData<WithData> source, InItData inIt, WithMemberInfo memberInfo, PageableInfo pageable) {
-		List<WithInfo> withInfos = new ArrayList<>();
-		for (WithData w : source.getData()) {
+			PageElements<With> withs, InIt inIt, WithMemberInfo memberInfo, PageRequest pageable) {
+		log.debug(
+				"on service assemble with info : w - {}, in - {}, m - {}",
+				withs.getTotalCount(),
+				inIt.getId(),
+				memberInfo.getName());
+		List<WithInfo> sources = new ArrayList<>();
+		for (With with : withs.getData()) {
 			Long hitCount = readHitCount(inIt);
-			WithInfo withInfo = makeWithInfo(w, hitCount, memberInfo);
-			withInfos.add(withInfo);
+			WithInfo withInfo = makeWithInfo(with, hitCount, memberInfo);
+			sources.add(withInfo);
 		}
-		return new PageImpl<>(withInfos, pageable, withInfos.size());
+		return new PageImpl<>(sources, pageable, sources.size());
 	}
 
-	private Long readHitCount(InItData inIt) {
+	private Long readHitCount(InIt inIt) {
 		return hitDao.countHitByInItAndPeriod(inIt.getId(), Period.today());
 	}
 
-	private WithInfo makeWithInfo(WithData w, Long hitCount, WithMemberInfo memberInfo) {
+	private WithInfo makeWithInfo(With with, Long hitCount, WithMemberInfo memberInfo) {
 		return WithInfo.builder()
-				.id(w.getId())
-				.content(w.getContent())
+				.id(with.getId())
+				.content(with.getContent())
 				.hit(hitCount)
 				.withMemberInfo(memberInfo)
 				.build();
