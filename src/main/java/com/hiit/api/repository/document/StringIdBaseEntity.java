e37ae5a29dee4008c25a2888e95c6b22baae78c0
belljun3395 <195850@jnu.ac.kr>
2024-01-06 16:13:06 +0900

    HIIT-031 feat: StringIdBaseEntity 구현

---
+++ src/main/java/com/hiit/api/repository/document/StringIdBaseEntity.java
@@ -0,0 +1,42 @@
+package com.hiit.api.repository.document;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@MappedSuperclass
+@EntityListeners({AuditingEntityListener.class})
+@SuperBuilder(toBuilder = true)
+public class StringIdBaseEntity implements Persistable<String> {
+
+	@Id private String id;
+
+	@Column(nullable = false, updatable = false)
+	@CreatedDate
+	private LocalDateTime createAt;
+
+	@Column(nullable = false)
+	@LastModifiedDate
+	private LocalDateTime updateAt;
+
+	@Override
+	public boolean isNew() {
+		return createAt == null && updateAt == null;
+	}
+}
