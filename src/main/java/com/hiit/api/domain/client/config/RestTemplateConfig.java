c6bfc5f437286e9534b36067a0cbc4105f04bcb3
belljun3395 <195850@jnu.ac.kr>
2024-01-06 16:56:23 +0900

    HIIT-031 feat: RestTemplateConfig 추가

---
+++ src/main/java/com/hiit/api/domain/client/config/RestTemplateConfig.java
@@ -0,0 +1,50 @@
+package com.hiit.api.domain.client.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@RequiredArgsConstructor
+public class RestTemplateConfig {
+
+	private final ClientHttpRequestInterceptor restTemplateClientHttpRequestInterceptor;
+	private final ClientHttpRequestInterceptor restTemplateLoggingInterceptor;
+
+	@Bean
+	public RestTemplate restTemplate(
+			@Value("${client.timeout.connect}") int connectTimeout,
+			@Value("${client.timeout.read}") int readTimeout,
+			@Value("${client.pool.max-connect}") int maxConnectPool,
+			@Value("${client.pool.max-connect-per-route}") int maxPerRouteConnectPool) {
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+		factory.setReadTimeout(readTimeout);
+		factory.setConnectTimeout(connectTimeout);
+		HttpClient httpClient =
+				HttpClientBuilder.create()
+						.setMaxConnTotal(maxConnectPool)
+						.setMaxConnPerRoute(maxPerRouteConnectPool)
+						.build();
+		factory.setHttpClient(httpClient);
+
+		RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(factory));
+		List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+		if (interceptors.isEmpty()) {
+			interceptors = new ArrayList<>();
+		}
+		interceptors.add(restTemplateClientHttpRequestInterceptor);
+		interceptors.add(restTemplateLoggingInterceptor);
+
+		restTemplate.setInterceptors(interceptors);
+		return restTemplate;
+	}
+}
