7452e3a69241dd265148f857375829890b73f9c3
belljun3395 <195850@jnu.ac.kr>
2024-01-06 16:16:30 +0900

    HIIT-031 feat: MemberStatDocRepository 선언

---
+++ src/main/java/com/hiit/api/repository/dao/document/MemberStatDocRepository.java
@@ -0,0 +1,6 @@
+package com.hiit.api.repository.dao.document;
+
+import com.hiit.api.repository.document.member.MemberStatDoc;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberStatDocRepository extends JpaRepository<MemberStatDoc, String> {}
