cc7bae618f53a5d5f86f2685887ea0146004d640
belljun3395 <195850@jnu.ac.kr>
2024-01-06 17:02:32 +0900

    HIIT-031 feat: MemberStatDoc 관련 메서드 추가 선언 및 구현

--- src/main/java/com/hiit/api/domain/dao/member/MemberDaoImpl.java
+++ src/main/java/com/hiit/api/domain/dao/member/MemberDaoImpl.java
@@ -2,7 +2,11 @@ package com.hiit.api.domain.dao.member;
 
 import com.hiit.api.domain.dao.AbstractJpaDao;
 import com.hiit.api.repository.dao.bussiness.HiitMemberRepository;
+import com.hiit.api.repository.dao.document.MemberStatDocRepository;
+import com.hiit.api.repository.document.member.ItWithStat;
+import com.hiit.api.repository.document.member.MemberStatDoc;
 import com.hiit.api.repository.entity.business.member.HiitMemberEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,10 +14,39 @@ import org.springframework.stereotype.Repository;
 public class MemberDaoImpl extends AbstractJpaDao<HiitMemberEntity, Long> implements MemberDao {
 
 	private final HiitMemberRepository repository;
+	private final MemberStatDocRepository memberStatDocRepository;
 
 	public MemberDaoImpl(
-			JpaRepository<HiitMemberEntity, Long> jpaRepository, HiitMemberRepository repository) {
+			JpaRepository<HiitMemberEntity, Long> jpaRepository,
+			HiitMemberRepository repository,
+			MemberStatDocRepository memberStatDocRepository) {
 		super(jpaRepository);
 		this.repository = repository;
+		this.memberStatDocRepository = memberStatDocRepository;
+	}
+
+	@Override
+	public Optional<HiitMemberEntity> findByCertificationId(String certificationId) {
+		return repository.findByCertificationId(certificationId);
+	}
+
+	@Override
+	public Optional<MemberStatDoc> findMemberStatDocByMemberId(Long memberId) {
+		return memberStatDocRepository.findById(memberId.toString());
+	}
+
+	@Override
+	public MemberStatDoc saveMemberStatDoc(MemberStatDoc memberStatDoc) {
+		return memberStatDocRepository.save(memberStatDoc);
+	}
+
+	@Override
+	public Optional<ItWithStat> findItWithStatByMemberIdAndItId(Long memberId, Long itId) {
+		Optional<MemberStatDoc> docs = memberStatDocRepository.findById(memberId.toString());
+		if (docs.isEmpty()) {
+			return Optional.empty();
+		}
+		MemberStatDoc source = docs.get();
+		return source.getResource().getItWithCountStats(itId);
 	}
 }
