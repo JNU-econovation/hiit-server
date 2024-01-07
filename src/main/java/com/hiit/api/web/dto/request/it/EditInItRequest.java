e5397aaa2d29c5c2267c82369c335abb073a137a
belljun3395 <195850@jnu.ac.kr>
2024-01-07 23:28:58 +0900

    HIIT-022 feat: EditInItRequest 추가

---
+++ src/main/java/com/hiit/api/web/dto/request/it/EditInItRequest.java
@@ -0,0 +1,25 @@
+package com.hiit.api.web.dto.request.it;
+
+import com.hiit.api.web.dto.validator.DataId;
+import com.hiit.api.web.dto.validator.DayCode;
+import com.hiit.api.web.dto.validator.ItResolution;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EditInItRequest {
+
+	@DataId private Long id;
+	@DayCode @NotNull private String dayCode;
+	@ItResolution @NotNull private String resolution;
+}
