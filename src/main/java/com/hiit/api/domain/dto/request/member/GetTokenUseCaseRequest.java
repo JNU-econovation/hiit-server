7c7bf3d29e4c6d44d47135027119ac15e30ac4ad
belljun3395 <195850@jnu.ac.kr>
2024-01-09 20:58:34 +0900

    HIIT-033 feat: GetTokenUseCaseRequest 추가

---
+++ src/main/java/com/hiit/api/domain/dto/request/member/GetTokenUseCaseRequest.java
@@ -0,0 +1,20 @@
+package com.hiit.api.domain.dto.request.member;
+
+import com.hiit.api.common.marker.dto.AbstractRequest;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class GetTokenUseCaseRequest implements AbstractRequest {
+
+	private String token;
+}
