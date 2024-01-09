6c819df535198ede6471433515d90ee652ce4335
belljun3395 <195850@jnu.ac.kr>
2024-01-09 20:34:21 +0900

    HIIT-035 feat: SocialSubject 추가

--- src/main/java/com/hiit/api/web/dto/request/member/SocialSubject.java
+++ src/main/java/com/hiit/api/web/dto/request/member/SocialSubject.java
@@ -2,9 +2,18 @@ package com.hiit.api.web.dto.request.member;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import java.io.Serializable;
+import lombok.Getter;
 
+@Getter
 public enum SocialSubject implements Serializable {
-	KAKAO;
+	KAKAO("kakao"),
+	;
+
+	private String value;
+
+	SocialSubject(String value) {
+		this.value = value;
+	}
 
 	// todo: JsonCreator 안쓰고 리펙토링 생각
 	@JsonCreator
