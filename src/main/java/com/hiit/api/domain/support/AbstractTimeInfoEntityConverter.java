096783f186a27090831726a6f67d069924a82506
belljun3395 <195850@jnu.ac.kr>
2024-01-10 10:21:02 +0900

    HIIT-036 refactor: support/conveter에 domain 변경 사항 반영

--- src/main/java/com/hiit/api/domain/support/AbstractTimeInfoEntityConverter.java
+++ src/main/java/com/hiit/api/domain/support/AbstractTimeInfoEntityConverter.java
@@ -2,11 +2,11 @@ package com.hiit.api.domain.support;
 
 import com.hiit.api.common.marker.entity.EntityMarker;
 import com.hiit.api.common.marker.model.AbstractDomain;
-import com.hiit.api.domain.model.ItTimeInfo;
+import com.hiit.api.domain.model.ItTimeDetails;
 
 /** 시간 정보가 포함된 엔티티와 도메인을 변환하는 인터페이스 */
 public interface AbstractTimeInfoEntityConverter<T extends EntityMarker, R extends AbstractDomain> {
 
 	/** 엔티티를 도메인으로 변환 */
-	R from(T t, ItTimeInfo timeInfo);
+	R from(T t, ItTimeDetails timeInfo);
 }
