577d7b940e120642e09f7da38cd9ddc7f5f281de
belljun3395 <195850@jnu.ac.kr>
2024-01-17 11:54:41 +0900

    HIIT-*** feat: 임시 프로필 랜덤 저장되도록 추가

--- src/main/java/com/hiit/api/domain/usecase/member/FacadeCreateMemberUseCase.java
+++ src/main/java/com/hiit/api/domain/usecase/member/FacadeCreateMemberUseCase.java
@@ -10,6 +10,7 @@ import com.hiit.api.domain.usecase.AbstractUseCase;
 import com.hiit.api.domain.usecase.member.event.CreateMemberEventPublisher;
 import com.hiit.api.repository.entity.business.member.CertificationSubject;
 import com.hiit.api.repository.entity.business.member.HiitMemberEntity;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class FacadeCreateMemberUseCase
 		implements AbstractUseCase<CreateSocialMemberUseCaseRequest> {
+
+	private final List<String> TEMP_PROFILE_IMAGES =
+			List.of(
+					"https://github.com/JNU-econovation/hiit-server/assets/102807742/b6bcc673-6e13-4da5-a32c-5e73c058c785",
+					"https://github.com/JNU-econovation/hiit-server/assets/102807742/135cd92a-60cd-4abe-ac6e-7f9619a8217d",
+					"https://github.com/JNU-econovation/hiit-server/assets/102807742/51a08eff-d365-4162-9454-7f3b47ebce4d",
+					"https://github.com/JNU-econovation/hiit-server/assets/102807742/2251673f-444f-4be2-a84e-267edc90bb4a");
 
 	private final CreateKaKaoMemberUseCase createKaKaoMemberUseCase;
 
@@ -59,6 +67,7 @@ public class FacadeCreateMemberUseCase
 						.certificationSubject(CertificationSubject.valueOf(certificationSubjectDetails.name()))
 						.nickName(nickname)
 						.certificationId(certificationId)
+						.profile(TEMP_PROFILE_IMAGES.get((int) (Math.random() * 4)))
 						.build();
 		Long memberId = dao.save(source).getId();
 		publisher.publish(memberId);
