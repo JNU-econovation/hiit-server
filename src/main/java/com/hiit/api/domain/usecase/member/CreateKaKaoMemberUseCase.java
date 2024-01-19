3d8c28ab27593e0a1be1268281970ac5f9d0829b
belljun3395 <195850@jnu.ac.kr>
2024-01-19 17:37:12 +0900

    HIIT-*** refactor: id 토큰 파싱하는 방법으로 변경

--- src/main/java/com/hiit/api/domain/usecase/member/CreateKaKaoMemberUseCase.java
+++ src/main/java/com/hiit/api/domain/usecase/member/CreateKaKaoMemberUseCase.java
@@ -2,11 +2,8 @@ package com.hiit.api.domain.usecase.member;
 
 import com.hiit.api.domain.client.member.token.RetryAbleKaKaoTokenClient;
 import com.hiit.api.domain.client.response.token.KaKaoIdTokenProperties;
-import com.hiit.api.domain.client.response.token.Token;
-import com.hiit.api.domain.exception.SocialIntegrationException;
 import com.hiit.api.domain.model.member.Member;
 import com.hiit.api.domain.service.token.KaKaoIdTokenParser;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,13 +17,13 @@ public class CreateKaKaoMemberUseCase {
 	private final KaKaoIdTokenParser kaKaoIdTokenParser;
 
 	public Member execute(final String code) {
-		Token token = kaKaoTokenClient.execute(code);
-		if (Objects.isNull(token.getToken())) {
-			throw new SocialIntegrationException();
-		}
-
-		String idToken = token.getToken();
-		KaKaoIdTokenProperties idTokenProperties = kaKaoIdTokenParser.parse(idToken);
+		//		Token token = kaKaoTokenClient.execute(code);
+		//		if (Objects.isNull(token.getToken())) {
+		//			throw new SocialIntegrationException();
+		//		}
+		//
+		//		String idToken = token.getToken();
+		KaKaoIdTokenProperties idTokenProperties = kaKaoIdTokenParser.parse(code);
 		String nickname = idTokenProperties.getNickname();
 		String certificationId = idTokenProperties.getSub();
 
